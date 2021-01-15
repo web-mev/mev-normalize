86a153fc0f5ff4ac1507c5bcf034e0562fa16c9e
blawney <blawney@hsph.harvard.edu>
2021-01-15 16:40:12 -0500

    Initial commit. Not tested in WebMEV environment  yet

---
+++ docker/normalize.R
@@ -0,0 +1,85 @@
+
+suppressMessages(suppressWarnings(library('rafalib', character.only=T, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library('metagenomeSeq', character.only=T, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library('edgeR', character.only=T, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library('DESeq2', character.only=T, warn.conflicts = F, quietly = T)))
+suppressMessages(suppressWarnings(library('matrixStats', character.only=T, warn.conflicts = F, quietly = T)))
+
+# args from command line:
+args<-commandArgs(TRUE)
+
+# path to the count matrix
+COUNTS_FILE <- args[1]
+
+# The method to use for normalization
+METHOD <- tolower(args[2])
+
+# whether or not to perform a log-transformation
+LOG_TRANSFORM <- as.logical(as.integer(args[3]))
+
+OUTPUT_FILE_PATH <- args[4]
+
+# Note that we assume there is no column header. This keeps R from 'renaming'
+# the columns of the matrix.
+counts <- read.table(COUNTS_FILE, sep='\t', stringsAsFactors=F, row.names=1)
+obj = newMRexperiment(counts, phenoData=NULL, featureData=NULL)
+#include this? don't think so.
+#obj = cumNorm(obj)
+
+METHODS <- c('css', 'tss', 'deseq2', 'tmm', 'uq')
+method_selection <- pmatch(tolower(METHOD), METHODS)
+if(is.na(method_selection)){
+    message('Invalid normalization method.')
+    quit(status=1)
+} else if(method_selection == -1){
+    message('Ambiguous transformation method')
+    quit(status=1)
+}
+
+if(METHOD == 'css'){
+    # gets the scaling factors so we can ultimately use that to set the 'global'
+    # expression level to the median of the adjusted counts
+    obj = cumNorm(obj)
+
+    # sl gives the scaling factor that is applied to all samples. It's not related
+    # the normalization per se, but rather to just adjusting the magnitude of all
+    # counts simultaneously
+    norm_mtx = MRcounts(obj,log=FALSE,norm=TRUE,sl=median(normFactors(obj)))
+}
+
+if(METHOD == 'tss'){
+    tss = colSums(counts, na.rm=T)
+    norm_mtx = sweep(counts,2,tss,'/')
+}
+
+if(METHOD == 'deseq2'){
+    deseq_sf = estimateSizeFactorsForMatrix(counts)
+    norm_mtx = sweep(counts,2,deseq_sf,'/')
+}
+
+if(METHOD == 'tmm'){
+  d <- DGEList(counts, lib.size = as.vector(colSums(counts, na.rm=T)))
+  d <- calcNormFactors(d,method='TMM')
+  norm_mtx = cpm(d, normalized.lib.size=TRUE)
+}
+
+if(METHOD == 'uq'){
+    y = as.matrix(counts) # otherwise matrixStats issues a warning
+    # only care about the expressed features. 
+    # This allows us to drop non-expressed when calculating the 75% percentile
+    y[y==0] = NA
+    scale = 1
+    normalizationScale = colQuantiles(y,p=0.75,na.rm=TRUE)
+    norm_mtx = sweep(counts,2,normalizationScale/scale,'/')
+}
+
+if(LOG_TRANSFORM && exists('norm_mtx')) {
+    norm_mtx = log2(norm_mtx+1)
+}
+
+# change the working directory to co-locate with the counts file:
+#working_dir <- dirname(COUNTS_FILE)
+#base_name = paste('normalized_counts', METHOD, 'tsv', sep='.')
+#results_file <- paste(working_dir, base_name, sep='/')
+# note that we do NOT write the header
+write.table(norm_mtx, file=OUTPUT_FILE_PATH, sep='\t', col.names=F, quote=FALSE)
---
+++ docker/run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# First arg is the path to the input counts matrix
+INPUT_COUNTS=$1
+
+OUTPUT_DIR=$(dirname $INPUT_COUNTS)
+
+# how to normalize (the method)
+METHOD=$2
+
+# bool- whether to log-transform the result
+LOG_TRANFORM=$3
+
+# To prevent R from mangling the column names, we send the counts (without header line)
+# to another temp file
+COUNTS_WITHOUT_HEADER="raw_counts.tsv"
+sed 1d $INPUT_COUNTS > $COUNTS_WITHOUT_HEADER
+
+# Call the R script. This will output a normalized count matrix without a header
+FOUT=$OUTPUT_DIR/"nc.tsv"
+Rscript /opt/software/normalize.R $COUNTS_WITHOUT_HEADER $METHOD $LOG_TRANFORM $FOUT
+
+# Create the final file by concatenating the original header (from the input matrix)
+# and the normalized counts (which did not have a header)
+if [ $LOG_TRANFORM = 1 ]
+then
+    FINAL_OUTPUT=$OUTPUT_DIR/"normalized_counts."$METHOD".log_transformed.tsv"
+else
+    FINAL_OUTPUT=$OUTPUT_DIR/"normalized_counts."$METHOD".tsv"
+fi
+head -1 $INPUT_COUNTS >$FINAL_OUTPUT
+cat $FOUT >>$FINAL_OUTPUT
+
+# Need to create the outputs.json file for WebMEV compatability
+echo "{\"normalized_counts\":\""$FINAL_OUTPUT"\"}" > outputs.json
+
+# cleanup
+rm $COUNTS_WITHOUT_HEADER
+rm $FOUT
